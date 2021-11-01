96be8badfe5204cba9283e0ed2b11b86a8933a50
James Burns <jrburns96@aol.com>
2021-11-01 15:57:22 -0400

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+function saturdayFun(activity="roller-skate"){
+return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity2="go to the office"){
+    return `This Monday, I will ${activity2}.`
+}
+function wrapAdjective(adjective="*"){
+    return function inner(singleparameter="special"){
+        return `You are ${adjective}${singleparameter}${adjective}!`;
+    }
+}
